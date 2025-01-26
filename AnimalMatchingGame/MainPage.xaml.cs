b1973ea3906a9c7718937048d4b27c7d5dce556d
Otis Singfield <singfieldotis@gmail.com>
2025-01-26 17:51:27 -0500

    initial code commit

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace AnimalMatchingGame;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void PlayAgainButton_Clicked(object sender, EventArgs e)
+	{
+		AnimalButtons.IsVisible = true;
+		PlayAgainButton.IsVisible = false;
+
+		List<string> animalEmoji =
+		[
+			"🐵", "🐵", 
+			"🐶", "🐶", 
+			"🐱", "🐱", 
+			"🐭", "🐭",
+			"🐸", "🐸",
+			"🐷", "🐷",
+			"🦉", "🦉",
+			"🦩", "🦩"
+		];
+
+		foreach (var button in AnimalButtons.Children.OfType<Button>())
+		{
+			int index = Random.Shared.Next(animalEmoji.Count);
+			string nextEmoji = animalEmoji[index];
+			button.Text = nextEmoji;
+			animalEmoji.RemoveAt(index);
+		}
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		
+	}
+}
+
