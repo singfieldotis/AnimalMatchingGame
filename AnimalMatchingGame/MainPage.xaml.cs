0dce95f85e17b13f6076959c0e2cfee415d0a842
Otis Singfield <singfieldotis@gmail.com>
2025-01-28 11:28:35 -0500

    Added logic for mathching tiles and fixed bug where counter would increment on blank tiles

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -34,9 +34,41 @@ public partial class MainPage : ContentPage
 		}
 	}
 
+	Button lastClicked;
+	bool findingMatch = false;
+	int matchesFound;
+
 	private void Button_Clicked(object sender, EventArgs e)
 	{
-		
+		if (sender is Button buttonClicked)
+		{
+			if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+			{
+				buttonClicked.BackgroundColor = Colors.Red;
+				lastClicked = buttonClicked;
+				findingMatch = true;
+			}
+			else
+			{
+				if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) &&
+				    (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+				{
+					matchesFound++;
+					lastClicked.Text = " ";
+					buttonClicked.Text = " ";
+				}
+				lastClicked.BackgroundColor = Colors.LightBlue;
+				buttonClicked.BackgroundColor = Colors.LightBlue;
+				findingMatch = false;
+			}
+		}
+
+		if (matchesFound == 8)
+		{
+			matchesFound = 0;
+			AnimalButtons.IsVisible = false;
+			PlayAgainButton.IsVisible = true;
+		}
 	}
 }
 
